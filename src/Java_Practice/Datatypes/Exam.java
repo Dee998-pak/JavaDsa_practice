187094528b37308926af937e16f417af35efda44
Dee998-pak <geniussitansu@gmail.com>
2026-09-01 23:20:09 +0530

    submit  30 percent of target

---
+++ src/Java_Practice/Datatypes/Exam.java
@@ -0,0 +1,16 @@
+package Java_Practice.Datatypes;
+
+public class Exam {
+    public static void main(String[] args){
+        int mark    =   339;
+        int mark1   =   239;
+        boolean passed  =   mark>=300;
+        boolean fail    =   mark<=300;
+
+        System.out.println("MARK:"+mark);
+        System.out.println("PASS:"+passed);
+        System.out.println();
+        System.out.println("MARK1:"+mark1);
+        System.out.println("FAIL:"+fail);
+    }
+}
---
+++ src/Java_Practice/Datatypes/StudentInfo.java
@@ -0,0 +1,23 @@
+package Java_Practice.Datatypes;
+
+public class StudentInfo {
+    public static void main(String[] args){
+
+        String  name    =   "Sitansu Behera";
+        int age =   27;
+        double cgpa    =   8.6;
+        String  course  =   "MCA";
+        char    grade   =   'A';
+        float   sgpa =   7.6f;
+
+        System.out.println("==============STUDENT INFORMATIONS:===============");
+        System.out.println("NAME:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("CGPA:"+cgpa);
+        System.out.println("COURSE:"+course);
+        System.out.println("GRADE:"+grade);
+        System.out.println("SGPA:"+sgpa);
+
+    }
+
+}
