a77e98d371b341324683a5ed9478e38ad9dfb715
Dee998-pak <geniussitansu@gmail.com>
2026-09-22 17:03:37 +0530

    submit it

--- src/JavaPractice/Datatypes/StringDemo.java
+++ src/JavaPractice/Datatypes/StringDemo.java
@@ -3,7 +3,9 @@ package JavaPractice.Datatypes;
 public class StringDemo {
     public static void main(String[] args) {
         String name = "Deepak Kumar behera";
-        String name1 = "Today, Ashis, Dada, and I were going to Bhubaneswar.";
+        String  name2    =   "Deepak Kumar behera";
+        String name1 = "        Today, Ashis, Dada, and I were going to Bhubaneswar.        ";
+        String name4    =   "           How do we create a String?          ";
         System.out.println(name);
         System.out.println(name1);
         System.out.println(name1.length());
@@ -33,6 +35,29 @@ public class StringDemo {
         System.out.println(name1.contains("Bhubaneswar"));
         System.out.println(name.contains("delhi"));
         System.out.println(name1.concat("@makt building."));
+        System.out.println();
+        System.out.println(name.charAt(7));
+        System.out.println();
+        System.out.println(name.length());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(8));
+        System.out.println();
+        System.out.println(name.substring(0,19));
+        System.out.println(name1.equals(name2));
+        System.out.println();
+        System.out.println(name.indexOf(19));
+        System.out.println(name2.replace("Deepak","sitansu"));
+        System.out.println(name4.toLowerCase());
+        System.out.println(name4.toUpperCase());
+        System.out.println(name4.trim());
+        System.out.println();
+
+        String[]words   =   name.split(" ");
+        System.out.println(words[0]);
+        System.out.println(words[1]);
+        System.out.println(words[2]);
+
+
 
     }
 }
